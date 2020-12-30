0d3e6ebe8aa7ddbae4a8c7cff15100f6246ff255
rami <ramyahmedsadek@gmail.com>
2020-12-30 20:15:12 +0200

    google maps

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,56 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-import 'home.dart';
+void main() => runApp(MyApp());
 
-void main() {
-  runApp(MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Google Maps Demo',
+      home: MapSample(),
+    );
+  }
 }
 
-class MyApp extends StatefulWidget {
-  // This widget is the root of your application.
+class MapSample extends StatefulWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  State<MapSample> createState() => MapSampleState();
 }
 
-class _MyAppState extends State<MyApp> {
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
 
-  // Set default `_initialized` and `_error` state to false
-  bool _initialized = false;
-  bool _error = false;
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
 
-  // Define an async function to initialize FlutterFire
-  void initializeFlutterFire() async {
-    try {
-      // Wait for Firebase to initialize and set `_initialized` state to true
-      await Firebase.initializeApp();
-      setState(() {
-        _initialized = true;
-      });
-    } catch(e) {
-      // Set `_error` state to true if Firebase initialization fails
-      setState(() {
-        _error = true;
-      });
-    }
-  }
-
-  @override
-  void initState() {
-    initializeFlutterFire();
-    super.initState();
-  }
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799, target: LatLng(37.43296265331129, -122.08832357078792), tilt: 59.440717697143555, zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
-
-    // Show error message if initialization failed
-    if(_error) {
-      return Center(
-        child: Text("error"),
-      );
-    }
-
-    // Show a loader until FlutterFire is initialized
-    if (!_initialized) {
-      return Center(
-        child: CircularProgressIndicator(),
-      );
-    }
-
-    return MaterialApp(
-      home: MyHomePage(),
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
     );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
